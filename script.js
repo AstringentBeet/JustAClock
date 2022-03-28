87e085794105ce9809e4d4202008f8218c9b4d20
Alexander Effanga <alexandereffanga@alexanders-air.attlocal.net>
2022-03-28 03:21:38 -0500

    Added asynchronous digital clock to demo page.

---
+++ script.js
@@ -0,0 +1,27 @@
+function clock(){
+    var date = new Date();
+    var h = date.getHours(); 
+    var m = date.getMinutes(); 
+    var s = date.getSeconds(); 
+    var meridium = "AM";
+    
+    if(h == 0){
+        h = 12;
+    }
+    
+    if(h > 12){
+        h = h - 12;
+        meridium = "PM";
+    }
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    
+    var time = h + ":" + m + ":" + s + " " + meridium;
+    document.getElementById("time").innerHTML = time;
+    setTimeout(clock, 1000);
+    
+}
+ 
+window.onload = clock();
